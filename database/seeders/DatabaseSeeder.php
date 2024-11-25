8307e3157ac52c7294a464811b88ce1af7467f68
Faisal Halim <ffaisalhalim@gmail.com>
2024-11-25 13:29:43 +0700

    :sparkles: feat: Add seeder for roles and permissions, and create multiple user types in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // call other seeders
+        $this->call(PermissionAndRoles::class);
 
-        User::factory()->create([
+        // ===== ===== ===== ===== =====  Administrator  User 
+        // Administrator user
+        $adm = User::factory()->create([
             'name'     => 'Administrator',
             'username' => 'admin',
             'email'    => 'admin@mail.com'
         ]);
+
+        // assign role
+        $adm->assignRole('administrator');
+
+        // ===== ===== ===== ===== =====  Komite Mutu User
+        $kmtu = [
+            [
+                'name'     => 'Komite Mutu 1',
+                'username' => 'komitemutu1',
+                'email'    => 'komitemutu1@mail.com'
+            ],
+            [
+                'name'     => 'Komite Mutu 2',
+                'username' => 'komitemutu2',
+                'email'    => 'komitemutu2@mail.com'
+            ],
+        ];
+
+        foreach ($kmtu as $user) {
+            $u = User::factory()->create($user);
+            $u->assignRole('komite-mutu');
+        }
+
+        // ===== ===== ===== ===== =====  Direksi User
+        $drksi = [
+            [
+                'name'     => 'Direksi 1',
+                'username' => 'direksi1',
+                'email'    => 'direksi1@mail.com'
+            ],
+            [
+                'name' => 'Direksi 2',
+                'username' => 'direksi2',
+                'email' => 'direksi2@mail.com',
+            ],
+        ];
+        
+        foreach ($drksi as $user) {
+            $u = User::factory()->create($user);
+            $u->assignRole('direksi');
+        }
+
+        // ===== ===== ===== ===== =====  Pelapor User
+        $u = User::factory(5)->create();
+        $u->each(function ($user) {
+            $user->assignRole('pelapor');
+        });
     }
 }
---
+++ database/seeders/PermissionAndRoles.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PermissionAndRoles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // package: spatie/laravel-permission
+        $permissionsAndRoles = [
+            'administrator' => [
+                'create_master_data',  // Membuat data master (penanggung biaya, jenis insiden, dll.)
+                'view_master_data',    // Melihat data master
+                'edit_master_data',    // Mengedit data master
+                'delete_master_data',  // Menghapus data master
+                'create_karyawan',     // Menambah data karyawan
+                'view_karyawan',       // Melihat data karyawan
+                'edit_karyawan',       // Mengedit data karyawan
+                'delete_karyawan',     // Menghapus data karyawan
+                'manage_roles_permissions',  // Mengatur role dan permission
+                'configure_system',    // Mengatur konfigurasi sistem
+                'view_all_reports',    // Melihat semua laporan insiden
+                'edit_all_reports',    // Mengedit semua laporan insiden
+                'delete_all_reports',  // Menghapus semua laporan insiden
+            ],
+            'komite-mutu' => [
+                'view_all_reports',          // Melihat semua laporan insiden
+                'analyze_reports',           // Menganalisis laporan berdasarkan grading risiko
+                'generate_recommendations',  // Memberikan rekomendasi terhadap insiden
+                'view_statistics',           // Melihat statistik insiden di seluruh unit
+            ],
+            'direksi' => [
+                'view_unit_reports',   // Melihat laporan insiden yang terkait dengan unitnya
+                'approve_actions',     // Memberikan persetujuan atau catatan untuk tindakan perbaikan
+            ],
+            'pelapor' => [
+                'create_report',       // Membuat laporan insiden
+                'view_own_reports',    // Melihat laporan yang dibuat oleh dirinya
+                'edit_own_reports',    // Mengedit laporan yang dibuat oleh dirinya
+                'delete_own_reports',  // Menghapus laporan yang dibuat oleh dirinya (opsional)
+            ],
+        ];
+
+        foreach ($permissionsAndRoles as $role => $permissions) {
+            foreach ($permissions as $permission) {
+                Permission::firstOrCreate(['name' => $permission]);
+            }
+
+            $r = Role::create(['name' => $role]);
+            $r->givePermissionTo($permissions);
+        }
+    }
+}

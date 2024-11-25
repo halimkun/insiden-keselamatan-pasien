4ad0fbdd6d973a1e6cef6ebd948df67726973764
Faisal Halim <ffaisalhalim@gmail.com>
2024-11-25 14:54:55 +0700

    :hammer: update: Reduce padding in user and dashboard views for improved layout consistency

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -21,7 +21,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
             <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
                 <div class="w-full">

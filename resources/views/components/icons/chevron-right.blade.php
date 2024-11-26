32bcd62d67d9621b742d481b8135e107efee1242
Faisal Halim <ffaisalhalim@gmail.com>
2024-11-26 08:31:54 +0700

    :sparkles: feat: Add SVG icons for back navigation and chevron directions

---
+++ resources/views/components/icons/chevron-right.blade.php
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {{ $attributes->merge(['class' => 'h-5 w-5']) }}>
+    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 6l6 6l-6 6" />
+</svg>

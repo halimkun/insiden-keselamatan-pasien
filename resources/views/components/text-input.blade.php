53515a38a2c1adcf3508e4941fa562860a7a906c
Faisal Halim <ffaisalhalim@gmail.com>
2024-11-28 11:15:52 +0700

    :hammer: update: Enhance text input component by adding conditional background color for disabled and readonly states

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -3,4 +3,11 @@
     'readonly' => false,
 ])
 
-<input @disabled($disabled) @readonly($readonly) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }} />
+<input 
+    @disabled($disabled) 
+    @readonly($readonly) 
+    {{ $attributes->merge(['class' => collect([
+        'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
+        ($disabled || $readonly) ? 'bg-gray-100' : ''
+    ])->filter()->implode(' ')]) }} 
+/>

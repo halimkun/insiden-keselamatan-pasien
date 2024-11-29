784360e37f5629a770a66411cb37ae2771769500
Faisal Halim <ffaisalhalim@gmail.com>
2024-11-29 11:02:39 +0700

    :sparkles: feat: Add new SVG icons for chevron down, hash, heart handshake, heart, restore, search, and user edit components

---
+++ resources/views/components/icons/restore.blade.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {{ $attributes->merge(['class' => 'h-5 w-5']) }}>
+    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <path d="M3.06 13a9 9 0 1 0 .49-4.087" />
+        <path d="M3 4.001v5h5M11 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0" />
+    </g>
+</svg>

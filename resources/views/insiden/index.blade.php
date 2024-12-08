cf26d36461430cc6aa93f3cb59ad5706845bd10f
Faisal Halim <ffaisalhalim@gmail.com>
2024-12-08 13:57:34 +0700

    :hammer: update: Refactor Insiden model to improve data handling and optimize performance; adjust related controller methods for consistency

--- resources/views/insiden/index.blade.php
+++ resources/views/insiden/index.blade.php
@@ -1,4 +1,19 @@
 <x-app-layout>
+    @push('styles')
+        {{-- using public url --}}
+        <link rel="stylesheet" href="{{ asset('static/css/dataTables.tailwindcss.css') }}">
+
+        <script src="{{ asset('static/js/dataTables.js') }}"></script>
+        <script src="{{ asset('static/js/dataTables.tailwindcss.js') }}"></script>
+
+        <style>
+            .dt-search {
+                display: none;
+                visibility: hidden;
+            }
+        </style>
+    @endpush
+
     <x-slot name="header">
         <h2 class="flex items-center gap-3 text-xl font-semibold leading-tight text-gray-800">
             <x-icons.alert-triangle class="h-5 w-5" />
@@ -16,7 +31,8 @@
                             <p class="mt-1 text-sm text-gray-600">Data insiden pasien yang terjadi di rumah sakit.</p>
                         </div>
                         <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                            <a type="button" href="{{ route('insiden.create', ['step' => '1']) }}" class="inline-flex items-center gap-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a type="button" href="{{ route('insiden.create', ['step' => '1']) }}"
+                                class="inline-flex items-center gap-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                 <x-icons.circle-plus class="h-5 w-5" />
                                 Add New
                             </a>
@@ -34,78 +50,44 @@
                                 </div>
 
                                 <div class="w-full overflow-x-auto lg:overflow-visible">
-                                    <table class="w-full divide-y divide-gray-300">
+                                    <table class="w-full divide-y divide-gray-300" id="tableData">
                                         <thead>
                                             <tr>
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">No</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    No</th>
 
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Insiden</th>
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Jenis</th>
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Tgl Kejadian</th>
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Dampak</th>
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Korban</th>
-                                                <th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Grading</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    Insiden</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    Jenis</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    Tgl Kejadian</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    Dampak</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    Korban</th>
+                                                <th scope="col"
+                                                    class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    Grading</th>
 
-                                                <th scope="col" class="px-3 py-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">#</th>
+                                                <th scope="col"
+                                                    class="px-3 py-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">
+                                                    #</th>
                                             </tr>
                                         </thead>
-                                        <tbody class="divide-y divide-gray-200 bg-white">
-                                            @foreach ($insidens as $insiden)
-                                                <tr class="even:bg-gray-50">
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ ++$i }}</td>
-
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $insiden->insiden }}</td>
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $insiden->jenisInsiden->alias }}</td>
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                        <p class="font-medium">{{ $insiden->tanggal_insiden->translatedFormat('D, d M Y') }}</p>
-                                                        <p class="text-xs">{{ $insiden->waktu_insiden }}</p>
-                                                    </td>
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ \Str::title($insiden->dampak_insiden) }}</td>
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ \Str::title($insiden->korban_insiden == 'lainnya' ? $insiden->korban_insiden_lainnya : $insiden->korban_insiden) }}</td>
-                                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                        @if (\Str::lower($insiden->grading->grading_risiko) == 'merah')
-                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                                                                {{ $insiden->grading->grading_risiko }}
-                                                            </span>
-                                                        @endif
-
-                                                        @if (\Str::lower($insiden->grading->grading_risiko) == 'kuning')
-                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
-                                                                {{ $insiden->grading->grading_risiko }}
-                                                            </span>
-                                                        @endif
-
-                                                        @if (\Str::lower($insiden->grading->grading_risiko) == 'hijau')
-                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                                {{ $insiden->grading->grading_risiko }}
-                                                            </span>
-                                                        @endif
-
-                                                        @if (\Str::lower($insiden->grading->grading_risiko) == 'biru')
-                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
-                                                                {{ $insiden->grading->grading_risiko }}
-                                                            </span>
-                                                        @endif
-                                                    </td>
-
-                                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900">
-                                                        <form action="{{ route('insiden.destroy', $insiden->id) }}" method="POST">
-                                                            <a href="{{ route('insiden.show', $insiden->id) }}" class="mr-2 font-bold text-gray-600 hover:text-gray-900">{{ __('Show') }}</a>
-                                                            <a href="{{ route('insiden.edit', $insiden->id) }}" class="mr-2 font-bold text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <a href="{{ route('insiden.destroy', $insiden->id) }}" class="font-bold text-red-600 hover:text-red-900" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;">{{ __('Delete') }}</a>
-                                                        </form>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
+                                        
                                     </table>
                                 </div>
 
-                                <div class="mt-4 px-4">
+                                {{-- <div class="mt-4 px-4">
                                     {!! $insidens->withQueryString()->links() !!}
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -114,7 +96,112 @@
         </div>
     </div>
 
+    {{-- Delete Confirmation --}}
+    <dialog id="confirmDelete" class="modal">
+        <div class="modal-box">
+            <form method="POST">
+                @csrf
+                @method('DELETE')
+
+                <h3 class="text-lg font-bold">Delete Confirmation !</h3>
+                <p class="text-sm">Are you sure you want to delete <span class="font-semibold" id="insiden"></span> ?</p>
+
+                <div class="modal-action mt-10 flex justify-end space-x-4">
+                    <x-danger-button onclick="confirmDelete.close()">Delete</x-danger-button>
+
+                    <form method="dialog">
+                        <x-secondary-button class="bg-red-500" onclick="confirmDelete.close()">Close</x-secondary-button>
+                    </form>
+                </div>
+            </form>
+        </div>
+
+        <form method="dialog" class="modal-backdrop">
+            <button>close</button>
+        </form>
+    </dialog>
+
     @push('scripts')
-        
+    <script>
+        $(document).ready(function() {
+            // delete-insiden on click get data-id
+            $(document).on('click', '.delete-insiden', function() {
+                $('#confirmDelete #insiden').text($(this).data('insiden'));
+                var url = "{{ route('insiden.destroy', ':id') }}".replace(':id', $(this).data('id'));
+                confirmDelete.showModal();
+                $('#confirmDelete form').attr('action', url);
+            });
+
+            // restore-insiden on click get data-id
+            // $(document).on('click', '.restore-insiden', function() {
+            //     $('#confirmRestore #restoreName').text($(this).data('insiden'));
+            //     var url = "{{ route('users.restore', ':id') }}".replace(':id', $(this).data('id'));
+            //     confirmRestore.showModal();
+            //     $('#confirmRestore form').attr('action', url);
+            // });
+
+            var table = $('#tableData').DataTable({
+                processing: true,
+                serverSide: true,
+                lengthChange: false,
+
+                pageLength: 10,
+
+                ajax: {
+                    url: "{{ route('datatables.insiden') }}",
+                    data: function(d) {
+                        d.show_deleted = $('#show_deleted').is(':checked') ? 1 : 0;
+                    },
+                    type: 'GET',
+                    // success: function(response) {
+                    //     console.log(response);
+                    // }
+                },
+
+                columns: [
+                    {
+                        data: 'id',
+                        name: 'id',
+                        orderable: false,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    {
+                        data: 'insiden',
+                        name: 'insiden',
+                    },
+                    {
+                        data: 'jenis_insiden.alias',
+                        name: 'jenis_insiden.alias',
+                    },
+                    {
+                        data: 'waktu__insiden',
+                        name: 'waktu__insiden',
+                    },
+                    {
+                        data: 'dampak_insiden',
+                        name: 'dampak_insiden',
+                    },
+                    {
+                        data: 'korban_insiden',
+                        name: 'korban_insiden'
+                    },
+                    {
+                        data: 'grading',
+                        name: 'grading',
+                    },
+                    {
+                        data: 'action',
+                        name: 'action'
+                    },
+                ],
+            });
+
+            $('#search').on('keyup', function() {
+                table.search(this.value).draw();
+            });
+        });
+    </script>
     @endpush
 </x-app-layout>

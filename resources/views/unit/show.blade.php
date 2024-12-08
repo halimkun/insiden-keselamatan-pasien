e65a619ef8d490dbfe704070466bff9cb379f55f
Faisal Halim <ffaisalhalim@gmail.com>
2024-12-08 12:22:40 +0700

    :hammer: update: Refactor HTML structure in unit show view for improved readability and formatting

--- resources/views/unit/show.blade.php
+++ resources/views/unit/show.blade.php
@@ -15,7 +15,8 @@
                             <p class="mt-1 text-sm text-gray-600">Detail Unit Kerja Rumah Sakit.</p>
                         </div>
                         <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                            <a type="button" href="{{ route('unit.index') }}" class="inline-flex items-center gap-2 rounded-md bg-gray-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+                            <a type="button" href="{{ route('unit.index') }}"
+                                class="inline-flex items-center gap-2 rounded-md bg-gray-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
                                 <x-icons.arrow-back class="h-5 w-5" />
                                 Kembali
                             </a>
@@ -27,11 +28,12 @@
                             <div class="inline-block min-w-full py-2 align-middle">
                                 <div class="mt-6 border-t border-gray-100">
                                     <dl class="divide-y divide-gray-100">
-                                        
-                                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                    <dt class="text-sm font-medium leading-6 text-gray-900">Nama Unit</dt>
-                                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $unit->nama_unit }}</dd>
-                                </div>
+
+                                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                            <dt class="text-sm font-medium leading-6 text-gray-900">Nama Unit</dt>
+                                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
+                                                $unit->nama_unit }}</dd>
+                                        </div>
 
                                     </dl>
                                 </div>

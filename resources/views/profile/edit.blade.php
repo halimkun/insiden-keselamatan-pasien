8fcd1502a15038ce76fc6dc6c42398455695663d
Faisal Halim <ffaisalhalim@gmail.com>
2024-12-16 05:51:35 +0700

    :hammer: Update: profile edit header to use localized text and add conditional delete user form; enhance user interface and functionality

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            Profil Pengguna
         </h2>
     </x-slot>
 
@@ -19,11 +19,13 @@
                 </div>
             </div>
 
+            @can('hapus_profil')
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="w-full">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
+            @endcan
         </div>
     </div>
 </x-app-layout>

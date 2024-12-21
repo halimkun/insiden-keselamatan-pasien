11f639c5680d829d0407441236ad6b209cf02f3d
Faisal Halim <ffaisalhalim@gmail.com>
2024-12-22 06:30:45 +0700

    :recycle: Refactor: user action buttons; extract to separate Blade component and implement authorization checks

--- app/DataTables/User.php
+++ app/DataTables/User.php
@@ -5,6 +5,7 @@ namespace App\DataTables;
 use App\Models\User as UserModel;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Blade;
 use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Services\DataTable;
@@ -32,7 +33,7 @@ class User extends DataTable
                             return '<span class="badge badge-primary lowercase font-semibold text-white">' . ucfirst($role) . '</span>';
                         case 'komite-mutu':
                             return '<span class="badge badge-secondary lowercase font-semibold text-white">' . ucfirst($role) . '</span>';
-                        case 'direksi':
+                        case 'tim-ikp':
                             return '<span class="badge badge-accent lowercase font-semibold text-white">' . ucfirst($role) . '</span>';
                         case 'pelapor':
                             return '<span class="badge badge-info lowercase font-semibold text-white">' . ucfirst($role) . '</span>';
@@ -49,52 +50,11 @@ class User extends DataTable
                 $showUrl = route('users.show', $user->id);
                 $editUrl = route('users.edit', $user->id);
 
-                $html = '<div class="flex items-center justify-center gap-3">';
-
-                $html .= '
-                        <a href="' . $showUrl . '" class="text-gray-600 hover:text-indigo-600" title="Lihat detail">
-                            ' . Blade::render('<x-icons.user-search class="h-[1rem] w-[1rem]" />') . '
-                        </a>
-                        <a href="' . $editUrl . '" class="text-gray-600 hover:text-indigo-600" title="Edit data">
-                            ' . Blade::render('<x-icons.user-edit class="h-[1rem] w-[1rem]" />') . '
-                        </a>
-                ';
-
-                $html .= '
-                    <div class="dropdown dropdown-left">
-                        <div tabindex="0" role="button" class="inline-flex items-center rounded-lg border px-1 py-1 text-center transition duration-150 ease-in-out hover:bg-indigo-600 hover:text-white">
-                            <div>
-                                ' . Blade::render('<x-icons.dots-vertical class="h-[0.9rem] w-[0.9rem]" />') . '
-                            </div>
-                        </div>
-                        <div tabindex="0" class="menu dropdown-content z-10 w-52 rounded-box border bg-base-100 p-2 shadow">
-                            <ul>
-                                <li>
-                                    <a href="' . route('users.roles', $user->id) . '" class="text-gray-600 hover:text-indigo-600">
-                                        ' . Blade::render('<x-icons.user-shild class="h-[1rem] w-[1rem]" />') . '
-                                        Role & Permission
-                                    </a>
-                                </li>
-                                <li>
-                                    ' . ( $user->deleted_at 
-                                        ? '<button class="text-green-600 hover:text-green-900 restore-user" data-id="' . $user->id . '" data-name="' . $user->name . '" onclick="confirmRestore.showModal()">
-                                            ' . Blade::render('<x-icons.restore class="h-[1rem] w-[1rem]" />') . '
-                                            Restore
-                                        </button>' 
-                                        : '<button class="text-red-600 hover:text-red-900 delete-user" data-id="' . $user->id . '" data-name="' . $user->name . '" onclick="confirmDelete.showModal()">
-                                            ' . Blade::render('<x-icons.trash class="h-[1rem] w-[1rem]" />') . '
-                                            Delete
-                                        </button>' 
-                                    ) . '
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                ';
-
-                $html .= '</div>';
-
-                return $html;
+                return view('components.actions.user-action', [
+                    'showUrl' => $showUrl,
+                    'editUrl' => $editUrl,
+                    'user' => $user
+                ]);
             })
 
             ->orderColumn('roles', function ($query, $order) {

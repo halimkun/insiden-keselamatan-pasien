bba1b262071c76b346ad6dc8091404798cbb9904
Faisal Halim <ffaisalhalim@gmail.com>
2025-02-11 11:17:33 +0700

    Update: Replace pig-money icon with moneybag icon in sidebar navigation

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -131,7 +131,7 @@
                     <li>
                         <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 {{ request()->routeIs('penanggung-biaya.index') ? 'bg-graydark dark:bg-meta-4' : '' }}"
                             href="{{ route('penanggung-biaya.index') }}">
-                            <x-icons.pig-money class="h-5 w-5" />
+                            <x-icons.moneybag class="h-5 w-5" />
 
                             Penanggung Biaya
                         </a>

627e6ccef40537768f87c0a9c7125556d1503e21
Faisal Halim <ffaisalhalim@gmail.com>
2025-02-15 06:51:07 +0700

    Update: Modify .gitignore to exclude all images, enhance guest layout with logo adjustments, and comment out forgot password route

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,14 +16,15 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
-                    <img src="{{ asset(\App\Helpers\SettingHelper::get('site_logo', 'images/logo.png')) }}" alt="Logo" class="w-[270px] max-h-[50px]" />
-                </a>
-            </div>
-
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <div class="flex items-center justify-between w-full sm:max-w-md mb-10">
+                    <a href="{{ env('APP_URL') }}">
+                        {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
+                        <img src="{{ asset(\App\Helpers\SettingHelper::get('site_logo', 'images/logo.png')) }}" alt="Logo" class="max-w-[270px] max-h-[50px]" />
+                    </a>
+                    <img src="{{ asset('images/umy-maadrs.jpg') }}" alt="Logo" class="max-w-[270px] max-h-[50px]" />
+                </div>
+
                 {{ $slot }}
             </div>
         </div>
